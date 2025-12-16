6a3813bad505adf01bf1ae32f52e1a9d740c399a
SteffanBurnette <burnettesteffansep7@gmail.com>
2025-12-16 18:43:50 -0500

    test

--- classificationTest.py
+++ classificationTest.py
@@ -4,6 +4,8 @@ from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 import streamlit as st #Ui Framework for ml applications
 
+#What is data?
+
 @st.cache_data 
 def load_data():
     iris = load_iris()
